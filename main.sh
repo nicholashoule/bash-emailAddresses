af71c6d0659b4044055bb9928d1cc819533d6742
Nicholas Houle <nicholashoule@users.noreply.github.com>
2022-03-09 11:38:01 -0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,6 +1,11 @@
-# The command that is executed when the run button is clicked.
-language = "bash"
-run = ["bash", "main.sh"]
-
-# The default file opened in the editor.
-entrypoint = "main.sh"
+#!/bin/bash
+# Write a bash script that finds all the invalid email addresses.
+#
+# Good Email Addresses:
+#egrep -io '\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b' input
+egrep -io '\b^[a-zA-Z0-9][A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b' input
+#
+# Bad Email Addresses:
+#grep -vE "^[A-Za-z][A-Za-z0-9._+-]+@[A-Za-z0-9-][A-Za-z0-9.-]+(\.?)[[:alpha:]]$" input
+#grep -vP "^[[:alpha:]][[:alnum:]-_+.]+@[[:alnum:]-]+\.[[:alnum:]-]+(\.?)[[:alpha:]]{2,6}$" input
+#egrep -iv '\b^[a-zA-Z0-9][A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b' input
